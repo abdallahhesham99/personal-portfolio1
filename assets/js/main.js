52fb0300d68b33d66e709622e9a57159b947485d
abdallahhesham99 <abdallahhesham414@gmail.com>
2022-05-09 19:54:10 +0200

    qualification section

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,8 @@ const skillsContent = document.getElementsByClassName("skills__content"),
 for (let i = 0; i < skillsHeader.length; i++) {
   //apply event on each skillsHeader
   skillsHeader[i].addEventListener("click", () => {
-    //i
+    //if className of skillsContent of i === close make it = open
+    // else make it close
     if (skillsContent[i].className === "skills__content skills__close") {
       skillsContent[i].className = "skills__content skills__open";
     } else {
@@ -32,3 +33,23 @@ for (let i = 0; i < skillsHeader.length; i++) {
     }
   });
 }
+// ========================QUALIFICATION==================================
+const qualificationButton = document.querySelectorAll(".qualification__button"),
+  qualificationContent = document.querySelectorAll(".qualification__content");
+
+qualificationButton.forEach((element, tabIndex) => {
+  element.addEventListener("click", function (e) {
+    //to remove all active class from tabs buttons
+    qualificationButton.forEach((element) => {
+      element.classList.remove("qualification__active");
+    });
+    //hide all qualification content
+    qualificationContent.forEach((element) => {
+      element.style.display = "none";
+    });
+    //add active class for current clicked button
+    e.currentTarget.classList.add("qualification__active");
+    //make qualification content of tabINDEX display block
+    qualificationContent[tabIndex].style.display = "block";
+  });
+});
