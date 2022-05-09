5a616e74f85e3dfe418b6e2160decf42c4143414
abdallahhesham99 <abdallahhesham414@gmail.com>
2022-05-09 08:51:10 +0200

    skills section (html-css-js)

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,19 @@ function linkAction() {
   navMenu.classList.remove("show-menu");
 }
 navLinks.forEach((navLink) => navLink.addEventListener("click", linkAction));
+
+// ============================ ACCORDION SKILLS ================================
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
+
+for (let i = 0; i < skillsHeader.length; i++) {
+  //apply event on each skillsHeader
+  skillsHeader[i].addEventListener("click", () => {
+    //i
+    if (skillsContent[i].className === "skills__content skills__close") {
+      skillsContent[i].className = "skills__content skills__open";
+    } else {
+      skillsContent[i].className = "skills__content skills__close";
+    }
+  });
+}
